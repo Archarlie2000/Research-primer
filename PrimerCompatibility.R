6c4a86c0b5c4b8231b6c18c3431dcab445e4da78
Archarlie2000 <cho19006@byui.edu>
2023-01-20 17:30:37 -0700

    Add diff in temp and GC contetn

--- PrimerCompatibility.R
+++ PrimerCompatibility.R
@@ -14,11 +14,12 @@ primers <- read.delim("output.txt", sep= '=', header = FALSE)
 # I only want the sequence of the left primer and the possible right primers
 primersFiltered <- primers[primers$V1 %in% c('SEQUENCE_ID', 'SEQUENCE_PRIMER', 'PRIMER_RIGHT_0_SEQUENCE'), ]
 
+#Recontructing the dataframe for Tm extraction
 getname <- data.frame(name = primersFiltered$V2[primersFiltered$V1 == "SEQUENCE_ID"])
 getseq1 <- data.frame(seq1 = primersFiltered$V2[primersFiltered$V1 == "SEQUENCE_PRIMER"])
 getseq2 <- data.frame(seq2 = primersFiltered$V2[primersFiltered$V1 == "PRIMER_RIGHT_0_SEQUENCE"])
 
-getTM <- qpcR:::cbind.na(list(getname$name), list(getseq1$seq1), list(getseq2$seq2), list(), list())  
+getTM <- qpcR:::cbind.na(getname$name, getseq1$seq1, getseq2$seq2, c("Na"))  
 
 for (i in 1:nrow(getTM)){
   if (!is.na(getTM[i,3])){
@@ -27,14 +28,13 @@ for (i in 1:nrow(getTM)){
   }
 }
 
+colnames(getTM) <- c("name", "seq", "seq2", "Tm")
+getTM <- as.data.frame(getTM)
 
-colnames(getTM) <- c("name", "seq", "seq2", "Tm", "Dup")
-class(getTM)
-getTM$array
 
-getTM$dup <- c(duplicated(getTM$Tm, fromLast = TRUE)  | duplicated(getTM$Tm))
+#Here is a list of temperature that our primers like
+unique(getTM$Tm)
 
-duplicated(getTM$Tm)
 
 
 # the primers data frame has a column for the attributes and a column for the values,
