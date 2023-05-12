57f7ccde7ded2811c75a5f6bdf0ac761d9313dbd
Archarlie2000 <cho19006@byui.edu>
2023-05-11 19:08:28 -0600

    did i stutter

--- Iguesswearebacktostreamlit/city.py
+++ Iguesswearebacktostreamlit/city.py
@@ -1,6 +1,12 @@
+import streamlit as st
 import rpy2.robjects as robjects
 import rpy2.robjects.packages as rpackages
-import streamlit as st
+import numpy
+from rpy2.robjects import r, pandas2ri
+import pandas as pd
+pandas2ri.activate()
+
+
 
 Bioc = rpackages.importr('BiocManager')
 BioM = rpackages.importr('biomaRt')
@@ -10,16 +16,29 @@ BioM = rpackages.importr('biomaRt')
 st.title("Add One to Your Number")
 
 # Create a numeric input field
-input_number = st.number_input("Enter a number:")
+name = st.text_input('Enter SNP ID: ', "rs1121980, rs9939609, rs7903146, rs7903146")
 
 
-output_number = robjects.r['pi']
-
-output_number = robjects.conversion.rpy2py(output_number)
-
-# Add 1 to the input value and display the result
-if input_number:
-    st.write("Input value:", input_number)
-    st.write("Output value:", output_number)
 
 
+robjects.r('''
+
+        f <- function(name) {
+    
+            snp_list <- strsplit(name, " ")[[1]]
+
+            snpmart <- useMart("ENSEMBL_MART_SNP", dataset = "hsapiens_snp")
+            snp_sequence <- getBM(attributes = c('refsnp_id', 'snp'),
+            filters = c('snp_filter', 'upstream_flank', 'downstream_flank'),
+            checkFilters = FALSE,
+            values = list(snp_list,  500, 500),
+            mart = snpmart,
+            bmHeader = TRUE)
+        }
+
+        ''')
+
+r_f = robjects.r['f']
+res  = pd.DataFrame(list(r_f(name)[1]))
+
+st.write('The snp sequnce ', res)
