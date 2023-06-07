0fb6af3783302a7f5f2bcda8d0f515d38c442c98
Archarlie2000 <cho19006@byui.edu>
2023-06-07 09:46:13 -0600

    New Evaluation function

--- Multiplexing App/dynamicprogrmaingfunction.R
+++ Multiplexing App/dynamicprogrmaingfunction.R
@@ -4,6 +4,7 @@ top <- 10
 threshold = 20
 level = 2
 final = matrix()
+valid_option = 0
 
 
 
@@ -42,18 +43,59 @@ list_2 = c(list(unique(get_list(3,2)[[1]])),
            list(unique(get_list(4,3)[[1]])))
 
 
+list_3 = c(list(unique(get_list(1,2)[[1]])), 
+           list(unique(get_list(1,3)[[1]])),
+           list(unique(get_list(2,2)[[1]])),
+           list(unique(get_list(2,3)[[1]])),
+           list(unique(get_list(3,2)[[1]])), 
+           list(unique(get_list(3,3)[[1]])),
+           list(unique(get_list(4,2)[[1]])),
+           list(unique(get_list(4,3)[[1]])))
+
+
+
+
+arranged_list <- list_3[order(sapply(list_3, length), 
+                                     decreasing = FALSE)]
+
+
+list_1 <- list(arranged_list[[1]])
+list_2 <- list(arranged_list[[2]])
+
+len_1 <- length(list_1[[1]])
+len_2 <- length(list_2[[1]])
+
+
+####
+list_1 <- output1
+list_2 <- output2
+####
+
 multiplex = function(list_1, list_2){
   if (length(list_1) != 0 && length(list_2) != 0 && level <=  le){
     level = level + 1
+    len_1 <- length(list_1[[1]])
+    print(len_1)
+    len_2 <- length(list_2[[1]])
+    print(len_2)
     combination <- expand.grid(append(list_1, list_2))
-    indices <- evaluation(combination)
+    indices <- evaluation_new(combination, len_1, len_2)
+    print(indices)
+    if (level == 2){
+    output1 <- append(list(unlist(list_1)[unlist(indices[1])]),
+                      list(unlist(list_2)[unlist(indices[2])]))
+    }
+    else
+    {
+      output1 <- append(list_1,
+                        list(unlist(list_2)[unlist(indices[2])]))
+    }
   
-    output1 <- append(list(list_1[[1]][indices[1][[1]]]),
-                      list(list_2[[1]][indices[2][[1]]]))
-  
-    output2 <- get_list(level,2)
-  
-    final = combination[indices, ]
+    print(output1)
+    output2 <- list(arranged_list[[level]])
+    print(output2)
+    
+    
     multiplex(output1, output2)
   
   return(indices)
@@ -64,51 +106,6 @@ multiplex = function(list_1, list_2){
 }
 
 
-evaluation <- function(combination){
-  num_rows <- nrow(combination)
-  num_cols <- ncol(combination)
-  permutation <- factorial(num_cols)/factorial(num_cols-2)/2
-  result_matrix <- matrix(0, nrow = num_rows, ncol = permutation)
 
-  
-  
-  # Iterate over each row of the matrix
-  start_time <- Sys.time()
-  for (i in 1:num_rows) {
-    row <- combination[i, ]
-    row <- lapply(row, as.character)
-    clock = 0
-    #print(paste("i is", i))
-    
-    for (j in 1:num_cols) {
 
-      #print(paste("j is", j))
-      if (max(result_matrix[i, ]) <= threshold){
-        for (k in j:num_cols) {
-          if (k>j){
-            #print(paste("k is", k))
-            clock = clock + 1
-            #print(paste("Order ----------- ", clock))
-            #print(row[[j]])
-            #print(row[[k]])
-            result_matrix[i, clock] <- calculate_dimer(row[[j]], row[[k]])$temp
-          }
-        }
-      }
-    }
-  }
-  end_time <- Sys.time()
-  print(elapsed_time <- end_time - start_time)
-  
-  result_matrix <- as.data.frame(result_matrix)
-  row_indices <- which(apply(result_matrix, 1, 
-                             function(row) all(row != 0)))
-  # indices_1 <- unique(ceiling(row_indices/len_1))
-  # indices_2 <- unique(sapply(row_indices, 
-  #                            function(x) if(x%%len_2 == 0){return(len_2)}
-  #                            else
-  #                              {return(x%%len_2)} ))
-  # print(indices_1)
-  # print(indices_2)
-  return(row_indices)
-}
+
