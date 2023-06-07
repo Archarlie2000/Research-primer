8cad30220c8aa4d9dd97d599d63b68fe721fcc8f
Archarlie2000 <cho19006@byui.edu>
2023-06-07 12:18:47 -0600

    need more mapping

--- Multiplexing App/dynamicprogrmaingfunction.R
+++ Multiplexing App/dynamicprogrmaingfunction.R
@@ -1,10 +1,9 @@
 
 
-top <- 10
-threshold = 20
+top <- 3
+threshold =  10
 level = 2
-final = matrix()
-valid_option = 0
+final = list()
 
 
 
@@ -21,7 +20,7 @@ df <- df2 %>%
   ungroup()
 
 nested_tables <- split(df, df$Identity)
-le <- length(nested_tables)
+levels <- length(nested_tables) *2
 
 
 get_list <- function(i, j){
@@ -30,17 +29,6 @@ get_list <- function(i, j){
   return(k)
 }
 
-# multiplex(get_list(1,2), get_list(2,2))
-
-list_1 = c(list(unique(get_list(1,2)[[1]])), 
-           list(unique(get_list(1,3)[[1]])),
-           list(unique(get_list(2,2)[[1]])),
-           list(unique(get_list(2,3)[[1]])))
-
-list_2 = c(list(unique(get_list(3,2)[[1]])), 
-           list(unique(get_list(3,3)[[1]])),
-           list(unique(get_list(4,2)[[1]])),
-           list(unique(get_list(4,3)[[1]])))
 
 
 list_3 = c(list(unique(get_list(1,2)[[1]])), 
@@ -53,10 +41,11 @@ list_3 = c(list(unique(get_list(1,2)[[1]])),
            list(unique(get_list(4,3)[[1]])))
 
 
-
-
 arranged_list <- list_3[order(sapply(list_3, length), 
                                      decreasing = FALSE)]
+
+
+print(arranged_list)
 
 
 list_1 <- list(arranged_list[[1]])
@@ -65,22 +54,27 @@ list_2 <- list(arranged_list[[2]])
 len_1 <- length(list_1[[1]])
 len_2 <- length(list_2[[1]])
 
+level = 3
 
 ####
 list_1 <- output1
 list_2 <- output2
 ####
 
+
+multiplex(list_1, list_2)
+
 multiplex = function(list_1, list_2){
   if (length(list_1) != 0 && length(list_2) != 0 && level <=  le){
     level = level + 1
     len_1 <- length(list_1[[1]])
-    print(len_1)
+    #print(len_1)
     len_2 <- length(list_2[[1]])
-    print(len_2)
+    #print(len_2)
     combination <- expand.grid(append(list_1, list_2))
+    print(paste("Total rows", level, nrow(combination)))
     indices <- evaluation_new(combination, len_1, len_2)
-    print(indices)
+    #print(indices)
     if (level == 2){
     output1 <- append(list(unlist(list_1)[unlist(indices[1])]),
                       list(unlist(list_2)[unlist(indices[2])]))
@@ -91,20 +85,79 @@ multiplex = function(list_1, list_2){
                         list(unlist(list_2)[unlist(indices[2])]))
     }
   
-    print(output1)
+    #print(output1)
     output2 <- list(arranged_list[[level]])
-    print(output2)
+    #print(output2)
     
+    final <- output1
     
     multiplex(output1, output2)
   
-  return(indices)
   }
   else{
-    return(NULL)
+    return(output1)
   }
 }
 
+
+######################
+# A for loop?
+
+
+arranged_list <- list_3[order(sapply(list_3, length), 
+                              decreasing = FALSE)]
+
+
+print(arranged_list)
+
+
+list_1 <- list(arranged_list[[1]])
+list_2 <- list(arranged_list[[2]])
+
+len_1 <- length(list_1[[1]])
+len_2 <- length(list_2[[1]])
+level <- 3
+
+for (level in 3:levels+1){
+  if (length(list_1) != 0 && length(list_2) != 0){
+    len_1 <- length(list_1)
+    print(paste("length 1:", len_1))
+    len_2 <- length(list_2[[1]])
+    print(paste("length 2:", len_2))
+    combination <- expand.grid(append(list_1, list_2))
+    print(paste("Total rows", level, "-------", nrow(combination)))
+    indices <- evaluation_new(combination, len_1, len_2)
+    # print(indices)
+    if (level == 3){
+      output1 <- append(list(unlist(list_1)[unlist(indices[1])]),
+                        list(unlist(list_2)[unlist(indices[2])]))
+    }
+    else
+    {
+      output1 <- append(list_1,
+                        list(unlist(list_2)[unlist(indices[2])]))
+    }
+    
+    
+    if (level == levels+1){
+    
+      final <- output1
+    }
+    else{
+      # print("output1")
+      # print(output1)
+      output2 <- list(arranged_list[[level]])
+      # print("output2")
+      # print(output2)
+      
+      list_1 <- output1
+      list_2 <- output2  
+      
+      
+      final <- output1
+    }
+  }
+}
 
 
 
--- Multiplexing App/multithreading.R
+++ Multiplexing App/multithreading.R
@@ -16,8 +16,12 @@ clusterEvalQ(cl, {
   library(stringr)
 })
 
+####
+
+good_slection = 0
+result_matrix = 0
 ###################
-evaluation <- function(combination){
+evaluation <- function(combination, len_1, len_2){
   num_rows <- nrow(combination)
   num_cols <- ncol(combination)
   permutation <- factorial(num_cols)/factorial(num_cols-2)/2
@@ -36,9 +40,11 @@ evaluation <- function(combination){
       #print(paste("j is", j))
       if (max(result_matrix[i, ]) <= threshold){
         for (k in j:num_cols) {
-          if (k>j){
-            clock = clock + 1
-            result_matrix[i, clock] <- calculate_dimer(row[[j]], row[[k]])$temp
+          if (max(result_matrix[i, ]) <= threshold){
+            if (k>j){
+              clock = clock + 1
+              result_matrix[i, clock] <- calculate_dimer(row[[j]], row[[k]])$temp
+            }
           }
         }
       }
@@ -46,17 +52,61 @@ evaluation <- function(combination){
   }
   
   result_matrix <- as.data.frame(result_matrix)
-  row_indices <- which(apply(result_matrix, 1, function(row) all(row < 5)))
-  # indices_1 <- unique(ceiling(row_indices/len_1))
-  # indices_2 <- unique(sapply(row_indices, 
-  #                            function(x) if(x%%len_2 == 0){return(len_2)}
-  #                            else
-  #                              {return(x%%len_2)} ))
-  # print(indices_1)
-  # print(indices_2)
-  return(result_matrix)
+  row_indices <- which(apply(result_matrix, 1, function(row) all(row < threshold)))
+  print(row_indices)
+  indices_1 <- unique(ceiling(row_indices/len_1))
+  indices_2 <- unique(sapply(row_indices,
+                             function(x) if(x%%len_2 == 0){return(len_2)}
+                             else
+                               {return(x%%len_2)} ))
+  print(indices_1)
+  print(indices_2)
+  good_slection = c(list(indices_1), list(indices_2))
+  return(good_slection)
 }
 
+
+########################################################
+# Evaluation new
+
+evaluation_new <- function(combination, len_1, len_2){
+  num_rows <- nrow(combination)
+  num_cols <- ncol(combination)
+  result_matrix <- matrix(0, nrow = num_rows, ncol = num_cols-1)
+  
+  
+  # Iterate over each row of the matrix
+  for (i in 1:num_rows) {
+    row <- combination[i, ]
+    row <- lapply(row, as.character)
+    clock = 0
+    
+    print(paste("i is", i))
+    for (j in 1:(num_cols-1)) {
+      
+      if (max(result_matrix[i, ]) <= threshold){
+        clock = clock + 1
+        result_matrix[i, clock] <- calculate_dimer(row[[j]], row[[num_cols]])$temp
+      }
+    }
+  }
+  
+  result_matrix <- as.data.frame(result_matrix)
+  row_indices <- which(apply(result_matrix, 1, function(row) all(row < threshold)))
+  print(row_indices)
+  indices_1 <- unique(ceiling(row_indices/len_1))
+  if (len_1 == 1){
+    indices_1 <- 1
+  }
+  indices_2 <- unique(sapply(row_indices,
+                             function(x) if(x%%len_2 == 0){return(len_2)}
+                             else
+                             {return(x%%len_2)} ))
+  print(indices_1)
+  print(indices_2)
+  good_slection = c(list(indices_1), list(indices_2))
+  return(good_slection)
+}
 
 ######################################################
 #Partition
@@ -98,9 +148,22 @@ for (i in 1:n) {
 
   end_time <- Sys.time()
   print(elapsed_time <- end_time - start_time)
-
 }
 
+
+#######################################
+# Single threading
+
+
+{
+start_time <- Sys.time()
+result <- evaluation(combination[1:5000, ])
+end_time <- Sys.time()
+print(elapsed_time <- end_time - start_time)
+}
+
+############################################
+#Clearning results
 
 remove_rows_with_threshold <- function(data, threshold) {
   filtered_data <- data %>%
@@ -120,5 +183,11 @@ cleaned_df <- remove_rows_with_threshold(combined_matrix, 5)
 
 
 
+####
 
+combined_matrix_filtered <- combined_matrix[combined_matrix[, ncol(combined_matrix)] != 0, ]
+row_indices <- which(apply(combined_matrix_filtered, 1, 
+                           function(row) all(row < 20)))
 
+length(row_indices)
+b <- apply(combined_matrix_filtered, 2, max)
