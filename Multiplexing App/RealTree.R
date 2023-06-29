ae55eb951b5cc67e07400266b1ed29464d0571b5
Archarlie2000 <cho19006@byui.edu>
2023-06-29 11:15:56 -0600

    figuire out

--- Multiplexing App/RealTree.R
+++ Multiplexing App/RealTree.R
@@ -31,16 +31,120 @@ for (i in names(newList)){
 }
 
 
-
-
 level3
-level2
-newList
+# level2
+# newList
+# level3
+
+level4 <- list()
+
+for (item in get_list(2,2)[[1]]) {
+  # Create a sublist with the name as the item
+  sublist <- list(1)
+  names(sublist) <- item
+  level4[item] <- sublist
+  }
+
+level4
+# level3
+# names(level3)
+# names(level3[[1]])
+
+# level5 <- list()
+
+
+ # level3[[1]][[1]] <- level4
+
+
+for (j in names(level3)){
+  for (k in names(level3[[j]])){
+    level3[[j]][[k]] <- level4
+  }
+}
+level3
+str(level3)
+
+# for (k in names(level3)){
+#   for (i in names(level3[[k]])){
+#     level5[k][i] <- list(level4)
+#   }
+# }
+
+level5
 
 
 
-# Add the sublist to the new list
-newList[[item]] <- sublist
+
+# compare the name of the current list at the end point 
+# to the parent node
+
+names(level3[[1]])[[1]]
+
+str(names(level3[[1]])[[1]])
+
+class(names(level3[[1]])[[1]])
+
+calculate_homodimer(names(level3[[1]])[[1]])$temp
+
+names(level3)[[1]]
+
+# Remove end point
+level3[[1]][[2]][3] <- NULL
 
 
-names(sublist) <- "GTAA"
+calculate_dimer(
+  names(level3)[[1]],
+  names(level3[[1]])[[1]]
+  )$temp
+
+
+
+# This function will compare everything come in to the last item
+
+compare <- function(s1,s2,s3){
+  
+  print(paste("comparision 2: ", calculate_dimer(s1, s3)$temp
+  ))
+  print(paste("Comparision 1: ", calculate_dimer(s2, s3)$temp
+  ))
+  result = ( calculate_dimer(s1, s3)$temp < threshold) +
+           ( calculate_dimer(s2, s3)$temp < threshold)
+  
+  return( (result == 0) )
+}
+
+
+
+# Evaluate parents
+for (j in 1:length(names(level3))){
+  for (k in 1:length(names(level3[[j]]))){
+    for (m in 1:length(names(level3[[j]][[k]]))){
+      
+      
+      print(paste("level 1: ", names(level3)[[j]], " ",j
+                  ))
+      print(paste("level 2: ", names(level3[[j]])[[k]], " ",k
+                  ))
+      print(paste("level 3: ", names(level3[[j]][[k]])[[m]], " ",m
+                  ))
+      
+      s1 <- names(level3)[[j]]
+      s2 <- names(level3[[j]])[[k]]
+      s3 <- names(level3[[j]][[k]])[[m]]
+      
+      if (compare(s1,s2,s3)){
+        print("not valid")
+      }
+      else{
+        print("valid")
+      }
+      print("------------")
+    }
+  }
+}
+
+
+
+
+
+
